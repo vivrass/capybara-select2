66c4eb03573f68febaa97beecd9ea2cbb6ea269a
Martin Provencher <mprovencher86@gmail.com>
2014-03-26 16:01:13 -0400

    1. Add support for Select2 version 3.4.5 which seems to need 'mousedown' instead of 'click' 2. Add support for Placeholder

--- gem/lib/capybara-select2.rb
+++ gem/lib/capybara-select2.rb
@@ -10,10 +10,21 @@ module Capybara
         select2_container = first(:xpath, options[:xpath])
       else
         select_name = options[:from]
-        select2_container = first("label", text: select_name).find(:xpath, '..').find(".select2-container")
+        label = first("label", text: select_name)
+        if label
+          select2_container = label.find(:xpath, '..').find(".select2-container")
+        else
+          # Check for placeholder
+          select2_container = first(".select2-chosen", text: select_name).find(:xpath, "../..")
+        end
       end
 
-      select2_container.find(".select2-choice").click
+      if Capybara.current_driver == :selenium
+        # trigger not supported by selenium
+        page.execute_script("$('##{select2_container[:id]} .select2-choice').trigger('mousedown')")
+      else
+        select2_container.find(".select2-choice").trigger(:mousedown)
+      end
 
       if options.has_key? :search
         find(:xpath, "//body").find("input.select2-input").set(value)
@@ -24,8 +35,10 @@ module Capybara
       end
 
       [value].flatten.each do |value|
-        select2_container.find(:xpath, "a[contains(concat(' ',normalize-space(@class),' '),' select2-choice ')] | ul[contains(concat(' ',normalize-space(@class),' '),' select2-choices ')]").click
-        find(:xpath, "//body").find("#{drop_container} li", text: value).click
+        # This failed the tests, why?
+        #select2_container.find(:xpath, "a[contains(concat(' ',normalize-space(@class),' '),' select2-choice ')] | ul[contains(concat(' ',normalize-space(@class),' '),' select2-choices ')]").click
+
+        find(:xpath, "//body").find("#{drop_container} li.select2-result-selectable", text: value).click
       end
     end
   end
